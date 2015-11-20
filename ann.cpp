02e67f8bfc84736ac964470c77017edee340510d
mriddell001 <matthew.s.riddell@gmail.com>
2015-11-20 12:26:04 -0800

    Create ann.cpp

---
+++ ann.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include "Node.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Node node;
+
+    double data = 0.0;
+    int dat = 0;
+    int layer = 0;
+    string tmp;
+
+    /** Input Source **/
+    ifstream in_file(argv[1], ios::in);
+    while(in_file >> data) {
+        node.Prime_input_vector(data);
+    }
+    /** Completed Input **/
+
+    /** Prime Hidden Layers **/
+    ifstream h_file(argv[2], ios::in);
+    while (h_file >> dat) {
+        node.Prime_hidden_layers(dat, layer);
+        layer++;
+    }
+    /** Completed Hidden Layers **/
+
+    /** Output Source**/
+    ifstream o_file(argv[3], ios::in);
+    while (o_file >> data) {
+        node.Prime_output_vector(data);
+    }
+    /** Completed Output **/
+
+    return 0;
+}
