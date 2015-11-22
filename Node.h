55eb1daed31c46a7a0ee0413c437f063e0c06f0e
mriddell001 <matthew.s.riddell@gmail.com>
2015-11-21 21:01:19 -0800

    Update Node.h
    Individual layers. No calculation functions yet.

--- Node.h
+++ Node.h
@@ -1,4 +1,5 @@
 #ifndef NODE_H
+#define NODE_H
 
 #include <string>
 #include <vector>
@@ -10,17 +11,25 @@ public:
     void Prime_input_vector(double);
     void Prime_output_vector(double);
     void Prime_hidden_layers(int, int);
+    void Connect_layers();
+    void Input_to_first_hidden();
+    void Hidden_to_hidden();
+    void Hidden_to_output();
 
 private:
+
+    class OutputNode {
+    public:
+        double original_value;
+    };
+    vector<OutputNode*> output_vector;
+
     class DataNode {
     public:
         int layer;
         double node_weight;
         vector<DataNode*> input_edges;
         vector<OutputNode*> output_edges;
-
-        DataNode();
-        ~DataNode();
     };
     vector<DataNode*> data_vector;
 
@@ -29,18 +38,8 @@ private:
         double original_value;
         vector<DataNode*> input_edges;
 
-        InputNode();
-        ~InputNode();
     };
     vector<InputNode*> input_vector;
-
-    class OutputNode {
-    public:
-        double original_value;
-        OutputNode();
-        ~OutputNode();
-    };
-    vector<OutputNode*> output_vector;
 
     vector<DataNode*> hidden_layers;
 };
