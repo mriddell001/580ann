2d262a4e0caa517b8a5fed304e6661f11dee6e0b
mriddell001 <matthew.s.riddell@gmail.com>
2015-11-27 19:04:56 -0800

    Update Node.cpp
    Completed updates to calculate and print output nodes.

--- Node.cpp
+++ Node.cpp
@@ -13,6 +13,7 @@ void Node::Prime_input_vector(double input) {
     InputNode *tmp = new InputNode();
     (*tmp).original_value = input;
     input_vector.push_back(tmp);
+    ind = 0;
 }
 
 void Node::Prime_hidden_layers(int input, int layers) {
@@ -21,16 +22,20 @@ void Node::Prime_hidden_layers(int input, int layers) {
     /** edge linking and weight setting.   **/
 
     DataNode *tmp = new DataNode();
-    (*tmp).node_weight = 1.0;
+    (*tmp).node_weight = 0.01;
     (*tmp).layer = layers;
+    (*tmp).index = ind;
     data_vector.push_back(tmp);
-    hidden_layers.push_back(tmp);
+    hidden_layers.push_back(ind);
+    ind++;
 
     for (int i = 1; i < input; i++) {
         DataNode *others = new DataNode();
-        (*tmp).node_weight = 1.0;
+        (*tmp).node_weight = 0.01;
         (*tmp).layer = layers;
+        (*tmp).index = ind;
         data_vector.push_back(tmp);
+        ind++;
     }
 }
 
@@ -55,6 +60,7 @@ void Node::Input_to_first_hidden() {
             DataNode *edge = *itt;
             tmp->input_edges.push_back(edge);
             ++itt;
+            (*tmp).edge_weight.push_back(0.5);
         }
     }
 }
@@ -67,11 +73,10 @@ void Node::Hidden_to_output() {
             for (int j = 0; j < output_vector.size(); j++) {
                 OutputNode *out = output_vector[i];
                 (*tmp).output_edges.push_back(out);
+                (*tmp).edge_weight.push_back(0.5);
             }
         }
-        else {
-            i = 0;
-        }
+        else {i = 0;}
     }
 }
 
@@ -81,16 +86,69 @@ void Node::Hidden_to_hidden() {
         /** This loop quits when it's in the last layer, to minimize work. **/
         if (data_vector[i]->layer != hidden_layers.size()-1) {
             int next = data_vector[i]->layer + 1;
-            DataNode *current = hidden_layers[i];
-            DataNode *tmp = hidden_layers[next];
-            DataNode *past = hidden_layers[next+1];
+        //  For each node in the hidden layer.
+            DataNode *current = data_vector[i];
+        //  Take the start of the next layer.
+            int start = hidden_layers[next];
+        //  Stop when you reach the layer after.
+            int past = hidden_layers[next+1];
+
         /** Return the first node in the next layer. Stops at the next layer. **/
-            for (vector<DataNode>::iterator it = tmp; it != past; ++it) {
-                current.input_edges.push_back((*it));
+            for (int j = start; j < past; j++) {
+                (*current).input_edges.push_back(data_vector[j]);
+                (*current).edge_weight.push_back(0.5);
             }
         }
         else {
             i = data_vector.size();
         }
+    }
+}
+
+void Node::Calculate_layers() {
+    // Preform activation from input layer.
+    for (int i = 0; i < input_vector.size(); i++) {
+        InputNode *tmp = input_vector[i];
+        for (int j = 0; j < (*tmp).edge_weight.size(); j++) {
+            DataNode *hidden_node = (*tmp).input_edges[j];
+            (*hidden_node).node_weight = (*hidden_node).node_weight + (*tmp).original_value * (*tmp).edge_weight[j];
+        }
+    }
+    // Calculate activation function.
+    for (int i = 0; i < data_vector.size(); i++) {
+        DataNode *tmp = data_vector[i];
+        double weight = (*tmp).node_weight;
+        weight = 1.0 / (1.0 + pow(e, (-weight)));
+        (*tmp).node_weight = weight;
+
+        // Preform activation across hidden layers.
+        if (i < (*hidden_layers.end())) {
+            for (int j = 0; j < (*tmp).edge_weight.size(); j++) {
+                DataNode *hidden_node = (*tmp).input_edges[j];
+                (*hidden_node).node_weight = (*hidden_node).node_weight + (*tmp).node_weight * (*tmp).edge_weight[j];
+            }
+        }
+        // Preform activation to output layer.
+        else {
+            for (int j = 0; j < (*tmp).edge_weight.size(); j ++) {
+                OutputNode *output_node = (*tmp).output_edges[j];
+                (*output_node).original_value = (*output_node).original_value + (*tmp).node_weight * (*tmp).edge_weight[j];
+            }
+        }
+    }
+
+    for (int i = 0; i < output_vector.size(); i++) {
+        OutputNode *tmp = output_vector[i];
+        double weight = (*tmp).original_value;
+        weight = 1.0 / (1.0 + pow(e, (-weight)));
+        (*tmp).original_value = weight;
+    }
+}
+
+void Node::Print_layers() {
+    for (int i = 0; i < output_vector.size(); i++) {
+        OutputNode *tmp = output_vector[i];
+        double weight = (*tmp).original_value;
+        cout << i << ": " << weight << endl;
     }
 }
