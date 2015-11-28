e987f0cdecf3744ee5b6127ffcddca0c9efab3cc
mriddell001 <matthew.s.riddell@gmail.com>
2015-11-27 19:05:53 -0800

    Update ann.cpp

--- ann.cpp
+++ ann.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <vector>
 #include "Node.h"
-#include "bmp.h"
+//#include "bmp.h"
 
 using namespace std;
 
@@ -12,19 +12,26 @@ int main(int argc, char *argv[])
 {
     Node node;
 
+    string tmp;
     double data = 0.0;
     int dat = 0;
     int layer = 0;
-    string tmp;
 
     /** Input Source **/
+    //  Expects a text file with input data in the form:
+    //  0.9 : Pixel is on.
+    //  0.1 : Pixel is off.
+
     ifstream in_file(argv[1], ios::in);
-    while(in_file >> data) {
+    while (in_file >> data) {
         node.Prime_input_vector(data);
     }
     /** Completed Input **/
 
     /** Prime Hidden Layers **/
+    //  Expects a text file with integers representing
+    //  the number of nodes in each hidden layer.
+
     ifstream h_file(argv[2], ios::in);
     while (h_file >> dat) {
         node.Prime_hidden_layers(dat, layer);
@@ -33,6 +40,9 @@ int main(int argc, char *argv[])
     /** Completed Hidden Layers **/
 
     /** Output Source**/
+    //  Expects a text file with output node layer data
+    //  including size of output and initial weight.
+
     ifstream o_file(argv[3], ios::in);
     while (o_file >> data) {
         node.Prime_output_vector(data);
@@ -40,6 +50,8 @@ int main(int argc, char *argv[])
     /** Completed Output **/
 
     node.Connect_layers();
+    node.Calculate_layers();
+    node.Print_layers();
 
     return 0;
 }
